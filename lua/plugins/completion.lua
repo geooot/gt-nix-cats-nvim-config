28d225c8e1bfd68a956fe7c103d803db7098e9fe
George Thayamkery <george.thayamkery@gmail.com>
2025-11-12 11:46:00 -0800

    change accept completion to be enter

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -3,7 +3,7 @@ require("blink.cmp").setup({
     preset = "default",
     ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
     ["<C-e>"] = { "hide" },
-    ["<C-y>"] = { "select_and_accept" },
+    ["<CR>"] = { "select_and_accept" },
     ["<C-p>"] = { "select_prev", "fallback" },
     ["<C-n>"] = { "select_next", "fallback" },
     ["<C-b>"] = { "scroll_documentation_up", "fallback" },
