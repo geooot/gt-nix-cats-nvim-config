ef87ece45a1dd397ae4837e0457f6a26c0ed959f
George Thayamkery <george.thayamkery@gmail.com>
2025-11-12 13:00:15 -0800

    fix enter button not working in insert mode

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -3,7 +3,7 @@ require("blink.cmp").setup({
     preset = "default",
     ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
     ["<C-e>"] = { "hide" },
-    ["<CR>"] = { "select_and_accept" },
+    ["<CR>"] = { "select_and_accept", "fallback" },
     ["<C-p>"] = { "select_prev", "fallback" },
     ["<C-n>"] = { "select_next", "fallback" },
     ["<C-b>"] = { "scroll_documentation_up", "fallback" },
